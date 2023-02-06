fbc314c45866d7f49bc57e46273e228bacf188ac
Jordan Dukes <jordandukes@Jordans-MacBook-Pro.local>
2023-02-05 23:04:57 -0800

    added simple get and post requests

--- db/run.js
+++ db/run.js
@@ -5,9 +5,17 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
+app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"))
-})
+    //read the db.json file and return all saved notes as JSON
+});
+
+app.post('/api/notes', (req, res) => {
+    //add it to the db.json
+    //return to the client
+});
+
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
